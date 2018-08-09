52466d67a3706dc51181ea971df7630fba3c57eb
Hillary Fan <hillary.fan123@gmail.com>
2018-08-08 18:13:31 -0700

    added comments

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+// required modules
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser');
-const app = express();
-const PORT = process.env.PORT || 5000
-const API_KEY = "bbMQ5abnmtXWaAw4oxCzndXmBVIWV77bxuJObPe1nYlETEdzNkdJncBeqBvSEyTqyUwJDaEcn4DYw9pOUa-Bp681KLt1Q15NY6b54iogbRS7nrb1JvtWGpikOkZqW3Yx";
-const CONVERT_MILES = 0.000621371192;
 var fetch = require('node-fetch');
+
+const app = express();
+
+// port for local production
+const PORT = process.env.PORT || 5000
+
+// api key for yelp
+const API_KEY = "bbMQ5abnmtXWaAw4oxCzndXmBVIWV77bxuJObPe1nYlETEdzNkdJncBeqBvSEyTqyUwJDaEcn4DYw9pOUa-Bp681KLt1Q15NY6b54iogbRS7nrb1JvtWGpikOkZqW3Yx";
+
+// const for converting meters to miles
+const CONVERT_MILES = 0.000621371192;
+
 // help to parse JSON sent from slack
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,8 +24,11 @@ app
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+
+  // response for slack command post request
   .post('/', (req, res) => {
 
+    // split the parameters by word 'within"
     var text = req.body.text.split("within");
     if (text[0]) { // TODO: luxury item - handle current location in the future
       var location = text[0];
@@ -26,6 +38,7 @@ app
     if (text[1]) {
       var radius = text[1];
     } else {
+      // default radius is 15 miles
       var radius = 15;
     }
 
@@ -41,15 +54,21 @@ app
     })
     .then(function(response) {
       response.json().then(function(data) { // need to convert to json for parsing
+        
+        // if businesses are found 
         if (data['total'] > 0) {
           var list_businesses = data['businesses'];
           var three_random = [];
+
+          // obtain three random businesses from business data
           while (three_random.length < 3) {
             let random_business = list_businesses[Math.floor(Math.random()*list_businesses.length)];
             if (!three_random.includes(random_business)) {
               three_random.push(random_business);
             } 
           }
+
+          // create json for slack response
           let responseJson = {
             response_type: 'in_channel',
             text: 'Here are three random options of restaurants within your area!',
@@ -57,6 +76,7 @@ app
               {
                 "title": three_random[0].name,
                 "title_link": three_random[0].url,
+                // TODO: clean up display of addresses for food trucks
                 "text": three_random[0].location.address1 + ", " + three_random[0].location.city,
                 'thumb_url': three_random[0].image_url,
                 "fields": [
@@ -126,6 +146,8 @@ app
             ]
           };
           res.status(200).send(res.json(responseJson));
+        } else {
+          res.status(200).send("No results found in specified location and given radius");
         }
       }).catch(function(err) {
         console.log(err)
